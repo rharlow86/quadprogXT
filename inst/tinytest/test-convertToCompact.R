35eb135e64e30267af5ffc3c2c1bbad74eb47c8a
ctrlxctrlc <ctrlxctrlc@proton.me>
2025-03-24 19:26:15 -0400

    Adding periods to test information strings.

--- inst/tinytest/test-convertToCompact.R
+++ inst/tinytest/test-convertToCompact.R
@@ -9,16 +9,16 @@ target <- list(Amat = AmatCompact, Aind = AindCompact)
 expect_equal(
   current = convertToCompact(Amat),
   target = target,
-  info = "Conversion to compact program yields same results as quadprog example"
+  info = "Conversion to compact program yields same results as quadprog example."
 )
 
 expect_error(
   current = convertToCompact(Amat * 0),
-  info = "Conversion to compact program fails when there is > 0 columns of all 0s"
+  info = "Conversion to compact program fails when there is > 0 columns of all 0s."
 )
 
 expect_equal(
   current = convertToCompact(AmatSparse)$Amat,
   target = target$AmatCompact,
-  info = "Conversion to compact program ignores unconstrained variables in Amat"
+  info = "Conversion to compact program ignores unconstrained variables in Amat."
 )
